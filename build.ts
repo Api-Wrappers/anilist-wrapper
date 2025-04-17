cecb6b47a892d90b68951dd417647c9ec30d9ad4
TDanks2000 <tommydanks2000@outlook.com>
2025-04-17 01:28:54 +0100

    build: enable minification and update package.json metadata
    Enable minification for both ESM and CJS builds to reduce bundle size. Additionally, update package.json with relevant metadata, including repository, keywords, author, and license information to improve package discoverability and maintainability.

--- build.ts
+++ build.ts
@@ -1,21 +1,23 @@
-import type { BuildConfig } from 'bun'
-import dts from 'bun-plugin-dts'
+import type { BuildConfig } from "bun";
+import dts from "bun-plugin-dts";
 
 const defaultBuildConfig: BuildConfig = {
-  entrypoints: ['./src/index.ts'],
-  outdir: './dist'
-}
+  entrypoints: ["./src/index.ts"],
+  outdir: "./dist",
+};
 
 await Promise.all([
   Bun.build({
     ...defaultBuildConfig,
     plugins: [dts()],
-    format: 'esm',
+    format: "esm",
     naming: "[dir]/[name].js",
+    minify: true,
   }),
   Bun.build({
     ...defaultBuildConfig,
-    format: 'cjs',
+    format: "cjs",
     naming: "[dir]/[name].cjs",
-  })
-])
+    minify: true,
+  }),
+]);
