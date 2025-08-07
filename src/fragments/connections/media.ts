63f5cb447d4e71b2b6ca99569c4d7bb7a300b98a
Tommy Danks <tommydanks2000@outlook.com>
2025-08-07 07:59:34 +0100

    feat: enhance MediaRelationNodeFragment with additional fields
    - Expanded the MediaRelationNodeFragment to include type, format, status, coverImage, startDate, endDate, season, seasonYear, episodes, chapters, volumes, duration, averageScore, popularity, favourites, genres, isAdult, countryOfOrigin, and siteUrl.
    - Improved the structure of media fragments by incorporating common fragments for cover images and dates, enhancing reusability and maintainability.

--- src/fragments/connections/media.ts
+++ src/fragments/connections/media.ts
@@ -1,13 +1,42 @@
 import { gql } from "graphql-request";
+import { COVER_IMAGE_FRAGMENT } from "../common/coverImage.js";
+import { DATE_FRAGMENT } from "../common/date.js";
 import { TITLE_FRAGMENT } from "../common/title.js";
 
 export const MEDIA_RELATION_NODE_FRAGMENT = gql`
   ${TITLE_FRAGMENT}
+  ${COVER_IMAGE_FRAGMENT}
+  ${DATE_FRAGMENT}
   fragment MediaRelationNodeFragment on Media {
     id
     title {
       ...TitleFragment
     }
+    type
+    format
+    status
+    coverImage {
+      ...CoverImageFragment
+    }
+    startDate {
+      ...DateFragment
+    }
+    endDate {
+      ...DateFragment
+    }
+    season
+    seasonYear
+    episodes
+    chapters
+    volumes
+    duration
+    averageScore
+    popularity
+    favourites
+    genres
+    isAdult
+    countryOfOrigin
+    siteUrl
   }
 `;
 
