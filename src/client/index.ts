f34aa7f27f95589d7e7a7a111cbc0c76fbae097d
Tommy Danks <tommydanks2000@outlook.com>
2025-08-07 14:08:54 +0100

    chore: update dependencies and remove unused ky package
    - Upgraded graphql-request to version 7.2.0 and related @graphql-codegen packages to their latest versions.
    - Removed the ky package and its associated fetch implementation from the client code, simplifying the GraphQL client setup.

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,27 +1,7 @@
 import { GraphQLClient } from "graphql-request";
-import ky from "ky";
 import { getSdk } from "../__generated__/anilist-sdk";
 
 const ANILIST_API_URL = "https://graphql.anilist.co";
-
-const kyFetchCore = async (
-	input: string | URL | Request,
-	init?: RequestInit,
-) => {
-	const response = await ky(input.toString(), {
-		method: init?.method,
-		headers: init?.headers,
-		body: init?.body,
-		credentials: "same-origin",
-		throwHttpErrors: false,
-	});
-	return response;
-};
-
-// Fix for graphql-request error preconnect missing when using ky instead of fetch
-const kyFetch: typeof fetch = Object.assign(kyFetchCore, {
-	preconnect: (input: string | URL): void => {},
-});
 
 export const createClient = (token?: string) => {
 	const headers: Record<string, string> = {
@@ -31,7 +11,7 @@ export const createClient = (token?: string) => {
 	if (token) headers.Authorization = `Bearer ${token}`;
 
 	const client = new GraphQLClient(ANILIST_API_URL, {
-		fetch: kyFetch,
+		fetch,
 		headers,
 	});
 
